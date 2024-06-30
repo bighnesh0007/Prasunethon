f67862739fac4094f69e5f602f415960dc5a67ad
bighnesh0007 <112768825+bighnesh0007@users.noreply.github.com>
2024-07-01 00:45:06 +0530

    Delete ai_mock - Copy directory

--- ai_mock - Copy/test-env.js
+++
@@ -1,3 +0,0 @@
-require('dotenv').config({ path: '.env.local' });
-
-console.log('NEXT_DRIZZLE_DATABASE_URL:', process.env.NEXT_DRIZZLE_DATABASE_URL);
--- ai_mock - Copy/utils/db.js
+++
@@ -1,8 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from './schema';
-
-
-
-const sql = neon('postgresql://ai-mock-interview_owner:0B9WxwtnmLSe@ep-noisy-firefly-a5pc6dvq.us-east-2.aws.neon.tech/ai-mock-interview?sslmode=require');
-export const db = drizzle(sql, { schema });
